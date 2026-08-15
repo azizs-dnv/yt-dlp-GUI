90217a22520e25d314c2adfff5f93a69e09d9c1f
Aziz <azizsadonov996@gmail.com>
2026-08-15 21:36:04 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+APP_NAME = "YT Downloader Pro"
+APP_VERSION = "2.0.0"
+DEFAULT_DOWNLOAD_DIR = str(Path.home() / "Downloads")
+HISTORY_FILE = Path.home() / ".yt_downloader_history.json"
